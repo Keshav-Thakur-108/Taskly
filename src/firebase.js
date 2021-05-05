3c55959cc9a1fc85327a8aa98410d0780bd2de87
Keshav <keshavthakur.001@gmail.com>
2021-05-05 16:45:59 +0530

    Add Tasks and Checkbox component and create App.scss

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
-import firebase from 'firebase/app'
-import 'firebase/firestore'
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
-const firebaseConfig = firebase.initializeApp = {
+const firebaseConfig = firebase.initializeApp({
     apiKey: "AIzaSyCzmyZU58Bex8DOKTTe9fsfeNDcComJb44",
     authDomain: "taskly-6ec78.firebaseapp.com",
     projectId: "taskly-6ec78",
     storageBucket: "taskly-6ec78.appspot.com",
     messagingSenderId: "199883151329",
     appId: "1:199883151329:web:967d18f0592e172ddbe4a0"
-}
+})
 
 export {firebaseConfig as firebase}
