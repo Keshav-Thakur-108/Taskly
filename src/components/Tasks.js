d76e4a71e8e1f6d4e5a95f72ebcd518672745766
Keshav <keshavthakur.001@gmail.com>
2021-05-19 18:32:36 +0530

    Fix tasks visibility issue

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -28,19 +28,18 @@ export const Tasks = () => {
         document.title = `${projectName}: Taskly`
     })
 
-    return (
-        <div className = "tasks" data-testid="tasks">
-            <h2 data-testid="project-name">{projectName}</h2>
-            <ul className="tasks__list">
-                {
-                    tasks.map(task => (
-                        <li key={`${task.id}`}>
-                            <Checkbox id={task.id}/>
-                            <span>{task.task}</span>
-                        </li>
-                    ))
-                }
-            </ul>
-        </div>
-    )
-}
+   return (
+    <div className="tasks" data-testid="tasks">
+      <h2 data-testid="project-name">{projectName}</h2>
+
+      <ul className="tasks__list">
+        {tasks.map((task) => (
+          <li key={`${task.id}`}>
+            <Checkbox id={task.id} taskDesc={task.task} />
+            <span>{task.task}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
