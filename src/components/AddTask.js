a1e98dc0444fb91e9f8c50a9308dd7f7e9c95605
Keshav <keshavthakur.001@gmail.com>
2021-05-27 17:37:20 +0530

    Add options for today, tomorrow and next week in taskDate component

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -4,6 +4,7 @@ import moment from 'moment'
 import {firebase} from '../firebase'
 import {useSelectedProjectValue} from '../context'
 import { ProjectOverlay } from './ProjectOverlay'
+import {TaskDate} from '../components/TaskDate'
 
 export const AddTask = ({
     showAddTaskMain = true, 
@@ -83,7 +84,7 @@ export const AddTask = ({
                     <ProjectOverlay setProject={setProject}
                     showProjectOverlay={showProjectOverlay}
                     setShowProjectOverlay={setShowProjectOverlay}/>
-                    <p>TaskDate here</p>
+                    <TaskDate setTaskDate={setTaskDate} showTaskDate ={showTaskDate} setShowTaskDate={setShowTaskDate}/>
                     <input className='add-task__content'
                     data-testid='add-task-content'
                     type='text'
