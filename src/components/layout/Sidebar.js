7cbe89496db4eca28ed0f26e92bf8782f4f496ae
Keshav <keshavthakur.001@gmail.com>
2021-05-29 14:39:41 +0530

    Make Sidebar component more accessible

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -19,29 +19,61 @@ const Sidebar = () => {
         <div className="sidebar">
             <ul className="sidebar__generic">
                 <li className={active === 'inbox' ? 'active' : undefined} data-testid="inbox" 
-                onClick={() => {setActive('inbox'); setSelectedProject('INBOX')}}
                 >
+                    <div
+                    onClick={() => {setActive('inbox'); setSelectedProject('INBOX')}}
+                    onKeyDown={() => {setActive('inbox'); setSelectedProject('INBOX')}}
+                    role="button"
+                    tabIndex={0}
+                    >
                     <span><FaInbox/></span>
                     <span>Inbox</span>
+                    </div>
                 </li>
                 <li className={active === 'today' ? 'active' : undefined} data-testid="today"
-                onClick={() => {setActive('today'); setSelectedProject('TODAY')}}
+                
                 >
+                    <div
+                    onClick={() => {setActive('today'); setSelectedProject('TODAY')}}
+                    onKeyDown={() => {setActive('today'); setSelectedProject('TODAY')}}
+                    role="button"
+                    tabIndex={0}
+                    >
                     <span><FaRegCalendar/></span>
                     <span>Today</span>
+                    </div>
                 </li>
                 <li className={active === 'next_7' ? 'active' : undefined} data-testid="next_7"
-                onClick={() => {setActive('next_7'); setSelectedProject('NEXT_7')}}
                 >
+                    <div
+                    onClick={() => {setActive('next_7'); setSelectedProject('NEXT_7')}}
+                    onKeyDown={() => {setActive('next_7'); setSelectedProject('NEXT_7')}}
+                    role="button"
+                    tabIndex={0}
+                    >
                     <span><FaRegCalendarAlt/></span>
                     <span>Next 7 days</span>
+                    </div>
                 </li>
             </ul>
 
-            <div className="sidebar__middle" onClick={() => setShowProjects(!showProjects)}>
-                <span><FaChevronDown className={!showProjects ? 'hidden-projects' : undefined}/></span>
-                <h2>Projects</h2>
-            </div>
+        <div
+        className="sidebar__middle"
+        aria-label="Show/hide projects"
+        onClick={() => setShowProjects(!showProjects)}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') setShowProjects(!showProjects);
+        }}
+        role="button"
+        tabIndex={0}
+      >
+        <span>
+          <FaChevronDown
+            className={!showProjects ? 'hidden-projects' : undefined}
+          />
+        </span>
+        <h2>Projects</h2>
+      </div>
 
             <ul className="sidebar__projects">
                {showProjects && <Projects/>}
