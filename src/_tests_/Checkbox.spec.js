6d4c4722b6d7a7717b3079b22ea7f38f7ad180f4
Keshav <keshavthakur.001@gmail.com>
2021-06-01 16:52:26 +0530

    Add test for Checkbox component

---
+++ src/_tests_/Checkbox.spec.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {render, cleanup, fireEvent} from '@testing-library/react'
+import {Checkbox} from '../components/Checkbox'
+
+beforeEach(cleanup) //Cleans the DOM
+
+jest.mock('../firebase', () => ({
+  firebase: {
+    firestore: jest.fn(() => ({
+      collection: jest.fn(() => ({
+        doc: jest.fn(() => ({
+          update: jest.fn(),
+        })),
+      })),
+    })),
+  },
+}));
+
+describe('<Checkbox />', () => {
+    describe('Success', () => {
+        it('renders the task checkbox', () => {
+            const {queryByTestId} = render(<Checkbox id = "1" taskDesc="Finish this tutorial series!"/>)
+            expect(queryByTestId('checkbox-action')).toBeTruthy();
+        })
+
+        it('renders the task checkbox and accepts a click', () => {
+            const {queryByTestId} = render(<Checkbox id = "1" taskDesc="Finish this tutorial series!"/>)
+            expect(queryByTestId('checkbox-action')).toBeTruthy();
+            fireEvent.click(queryByTestId('checkbox-action'))
+        })
+
+        it('renders the task checkbox and accepts a keyDown', () => {
+            const {queryByTestId} = render(<Checkbox id = "1" taskDesc="Finish this tutorial series!"/>)
+            expect(queryByTestId('checkbox-action')).toBeTruthy();
+            fireEvent.keyDown(queryByTestId('checkbox-action'))
+        })
+
+        
+
+        
+    })
+})
